9f987f411a671a90ec6553f403e05dd9acfa430c
ImTheOryo <enory.dhuysser@coda-student.school>
2026-06-02 16:57:40 +0200

    Fix : Setup to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
     optimizeDeps: {
       force: true,
     },
